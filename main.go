2de5a5b353a37ab5c0941f49fa6049de250ca53d
Fabio Dantas Fernandes <nunseik@gmail.com>
2025-08-05 17:47:40 -0300

    Add user management functionality with database schema and queries

--- main.go
+++ main.go
@@ -1,20 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/nunseik/go-server/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries *database.Queries
 }
 
 
 
 func main() {
 	const port = "8080"
-	apiCfg := apiConfig{}
+
+	godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("error opening sql db: %s", err)
+	}
+
+	dbQueries := database.New(db)
+
+
+	apiCfg := apiConfig{
+		dbQueries: dbQueries,
+	}
 	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
