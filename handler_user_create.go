63b55f5ce9552bceba02bc779c51b70a72fd8cc8
Fabio Dantas Fernandes <nunseik@gmail.com>
2025-08-06 08:50:25 -0300

    refactored handlerUserCreation

---
+++ handler_user_create.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+func (cfg *apiConfig) handlerUserCreation(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, "error decoding parameters", err)
+		return
+	}
+	
+	user, err := cfg.dbQueries.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		respondWithError(w, 500, "error creating user", err)
+	}
+
+	respondWithJSON(w, 201, User{
+		ID: user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email: user.Email,
+	})
+}
--- reset.go
+++ reset.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
-	"time"
-
-	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
@@ -25,35 +21,3 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hits reset to 0"))
 	}
 
-type User struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Email     string    `json:"email"`
-}
-
-func (cfg *apiConfig) handlerUserCreation(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Email string `json:"email"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		respondWithError(w, 500, "error decoding parameters", err)
-		return
-	}
-	
-	user, err := cfg.dbQueries.CreateUser(r.Context(), params.Email)
-	if err != nil {
-		respondWithError(w, 500, "error creating user", err)
-	}
-
-	respondWithJSON(w, 201, User{
-		ID: user.ID,
-		CreatedAt: user.CreatedAt,
-		UpdatedAt: user.UpdatedAt,
-		Email: user.Email,
-	})
-}
